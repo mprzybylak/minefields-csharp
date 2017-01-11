6dd346ac2e8359a665a3352de9c0a61c961dda97
Michał Przybylak <michal.przybylak@gmail.com>
2017-01-11 21:17:44 +0100

    hello world init commit

---
+++ helloworld/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pl.mprzybylak.miefields.csharp.helloworld
+{
+	public class HelloWorld
+	{
+		public HelloWorld()
+		{
+			
+		}
+
+		static void Main()
+		{
+			System.Console.WriteLine("Hello, World!");
+		}
+
+	}
+}
+
