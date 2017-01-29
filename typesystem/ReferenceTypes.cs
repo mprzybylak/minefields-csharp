ca7e6f67e54319cea238045e16a6a47ff8cc8bb1
Michał Przybylak <michal.przybylak@gmail.com>
2017-01-29 20:12:20 +0100

    simple example of reference types

---
+++ typesystem/ReferenceTypes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace typesystem
+{
+	public static class ReferenceTypes
+	{
+		public static void example()
+		{
+			Console.WriteLine("Regerence types");
+			references();
+			passingReferencesToMethods();
+			Console.WriteLine();
+		}
+
+		private static void references()
+		{
+			SimplestClass firstObject = new SimplestClass();
+			Console.WriteLine("We've created object {0}", firstObject);
+
+			// multiple references can point to the same object
+			SimplestClass otherReferenceToFirstObject = firstObject;
+			Console.WriteLine("We've created other reference to the same object {0} - we cannot do that with value types", otherReferenceToFirstObject);
+		}
+
+		private static void passingReferencesToMethods()
+		{
+			SimplestClass simplestClass = new SimplestClass();
+			Console.WriteLine("Object created: {0}", simplestClass.GetHashCode());
+
+			assignNewValueToPassedReference(simplestClass);
+			Console.WriteLine("Do our reference changed after exiting from method? {0}", simplestClass.GetHashCode());
+		}
+
+		private static void assignNewValueToPassedReference(SimplestClass reference)
+		{
+			reference = new SimplestClass();
+			Console.WriteLine("Method re-asigned reference {0}", reference.GetHashCode());
+		}
+
+	}
+
+	public class SimplestClass
+	{
+		public string text;
+	}
+}
--- typesystem/TypeSystem.cs
+++ typesystem/TypeSystem.cs
@@ -7,10 +7,14 @@ namespace typesystem
 	{
 		public static void Main()
 		{
+			// value types
 			ValueTypesInitialization.example();
 			ValueTypeAliases.example();
 			UserDefinedStructs.example();
 			UserDefinedEnums.example();
+
+			// reference types
+			ReferenceTypes.example();
 		}
 	}
 }
